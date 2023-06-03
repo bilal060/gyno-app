666fa7c36ab57464c1abf83fc5b6de973ab74e9f
Muhammad Bilal <muhammad.bilal@evocs.tech>
2023-06-03 19:13:38 +0400

    changes

--- src/pages/CallApi.js
+++ src/pages/CallApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = process.env.API_URL || "http://localhost:3000/api/v1";
+const url = process.env.API_URL || "http://localhost:5000/api/v1";
 
 const callAPI = async (endpoint) => {
   try {
